ac177ca61febfba19553b333f12ab9a0a64e542e
Calvinbullock <calvinnbullock@gmail.com>
2023-10-06 12:48:52 -0700

    added alies appends to myinstalls and lm sensors install

--- myinstalls.sh
+++ myinstalls.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+######## Basic package installs 
 # sudo apt install flatpak
 # sudo flatpak install krita
 # sudo apt install btop
 sudo apt install neovim
 
+######## Appended alies to .bashrc #########
+# Update alias
+echo "# Custom update alias_updater" >> .bashrc
+echo "alias upd=\"sudo apt update && sudo apt upgrade && flatpak update\"" >> .bashrc
+echo "" >> .bashrc
+
+# install temps package and alies my most used command from it
+sudo apt install lm-sensors
+echo "# Custom tempature sensors" >> .bashrc
+echo "alias temps=\"watch sensors" >> .bashrc
 
