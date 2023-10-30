6ead5fb5b27f4089524e1518e95ce4304f84fc4a
Calvinbullock <calvinnbullock@gmail.com>
2023-10-29 17:34:27 -0700

    added more alies to bashrc

--- myinstalls.sh
+++ myinstalls.sh
@@ -6,17 +6,26 @@ sudo apt update && sudo apt upgrade -y
 ######## Basic package installs 
 # sudo apt install flatpak
 # sudo flatpak install krita
-# sudo apt install btop
+sudo apt install htop
+sudo snap install btop
+sudo apt install upscaler
 sudo apt install neovim
+
 
 ######## Appended alies to .bashrc #########
 # Update alias
 echo "# Custom update alias_updater" >> .bashrc
-echo "alias upd=\"sudo apt update && sudo apt upgrade && flatpak update\"" >> .bashrc
+echo "alias upd=\"sudo apt update && sudo apt upgrade && flatpak update\n\"" >> .bashrc
 echo "" >> .bashrc
+echo "\n" >> .bashrc
 
 # install temps package and alies my most used command from it
 sudo apt install lm-sensors
-echo "# Custom tempature sensors" >> .bashrc
-echo "alias temps=\"watch sensors" >> .bashrc
+echo "# Custom tempature sensors\n" >> .bashrc
+echo "alias temps=\"watch sensors\n\"" >> .bashrc
+
+# Vim config alias
+echo "\n" >> .bashrc
+echo "alias vimcon=\"~/vim ~/.vim/vimrc\n\"" >> .bashrc
+echo "alias nimcon=\"vim ~/.config/nvim\n\"" >> .bashrc
 
