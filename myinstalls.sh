1b86ca3c1eebdd4422071f9680597812a6d1cd10
Calvinbullock <calvinnbullock@gmail.com>
2023-11-04 20:37:20 -0700

    added a variabe for .bashrc

--- myinstalls.sh
+++ myinstalls.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#bashrc="~/.bashrc"
+bashrc="~/Desktop/bashrk"
+
 # update mirrirs and system
 sudo apt update && sudo apt upgrade -y
 
@@ -11,28 +14,28 @@ sudo flatpak install com.discordapp.Discord
 sudo flatpak install io.gitlab.theevilskeleton.Upscaler
 
 sudo snap install btop
+#sudo snap install neovim
 
 sudo apt install htop
-sudo apt install neovim
 
 
-######## Appended alies to .bashrc #########
+######## Appended alies to $bashrc #########
 # Update alias
-echo "# Custom update alias_updater\n" >> .bashrc
-echo "alias upd=\"sudo apt update && sudo apt upgrade && flatpak update\n\"" >> .bashrc
-echo "" >> .bashrc
-echo "\n" >> .bashrc
+echo "# Custom update alias_updater\n" >> $bashrc
+echo "alias upd=\"sudo apt update && sudo apt upgrade && flatpak update\n\"" >> $bashrc
+echo "" >> $bashrc
+echo "\n" >> $bashrc
 
 # install temps package and alies my most used command from it
 sudo apt install lm-sensors
-echo "# Custom tempature sensors\n" >> .bashrc
-echo "alias temps=\"watch sensors\n\"" >> .bashrc
+echo "# Custom tempature sensors\n" >> $bashrc
+echo "alias temps=\"watch sensors\n\"" >> $bashrc
 
 # Vim config alias
-echo "\n" >> .bashrc
-echo "alias vimcon=\"vim ~/.vim/vimrc\n\"" >> .bashrc
-echo "alias govimcon=\"cd ~/.vim/\n\"" >> .bashrc
-echo "alias nimcon=\"vim ~/.config/nvim\n\"" >> .bashrc
-echo "alias gonimcon=\"cd ~/.config/nvim\n\"" >> .bashrc
-echo "alias nimcon=\"vim ~/.config/nvim\n\"" >> .bashrc
+echo "\n" >> $bashrc
+echo "alias vimcon=\"vim ~/.vim/vimrc\n\"" >> $bashrc
+echo "alias govimcon=\"cd ~/.vim/\n\"" >> $bashrc
+echo "alias nimcon=\"vim ~/.config/nvim\n\"" >> $bashrc
+echo "alias gonimcon=\"cd ~/.config/nvim\n\"" >> $bashrc
+echo "alias nimcon=\"vim ~/.config/nvim\n\"" >> $bashrc
 
