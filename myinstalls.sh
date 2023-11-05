7c39d9d301505db9bd5f24bc45e8ab231df43889
Calvinbullock <calvinnbullock@gmail.com>
2023-11-04 21:04:08 -0700

    added echos  what is installing to user

--- myinstalls.sh
+++ myinstalls.sh
@@ -8,15 +8,19 @@ sudo apt update && sudo apt upgrade -y
 
 # ####### Basic package installs 
 # sudo apt install flatpak
+echo "install krita, flatseal, discord, upscaler"
 sudo flatpak install org.kde.krita
-sudo flatpak unstall com.github.tchx84.Flatseal
+sudo flatpak install com.github.tchx84.Flatseal
 sudo flatpak install com.discordapp.Discord
 sudo flatpak install io.gitlab.theevilskeleton.Upscaler
 
+echo "install botpop, nvim"
 sudo snap install btop
 #sudo snap install neovim
 
+echo "install htop, lm-sensors"
 sudo apt install htop
+sudo apt install lm-sensors
 
 
 ######## Appended alies to $bashrc #########
@@ -27,7 +31,6 @@ echo "" >> $bashrc
 echo "\n" >> $bashrc
 
 # install temps package and alies my most used command from it
-sudo apt install lm-sensors
 echo "# Custom tempature sensors\n" >> $bashrc
 echo "alias temps=\"watch sensors\n\"" >> $bashrc
 
